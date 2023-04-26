db0620ccaaaeac26c57387620e47786fa306b5aa
Togzhan <t_kali@kbtu.kz>
2023-04-26 17:15:48 +0600

    upd

--- front/src/app/goverment/goverment.component.ts
+++ front/src/app/goverment/goverment.component.ts
@@ -1,18 +1,18 @@
 import { Component,OnInit} from '@angular/core';
-import { GovListService} from '../service/gov-list/gov-list.service';
+// import { GovListService} from '../service/gov-list/gov-list.service';
 
 @Component({
   selector: 'app-goverment',
   templateUrl: './goverment.component.html',
   styleUrls: ['./goverment.component.css']
 })
-export class GovermentComponent implements OnInit {
+export class GovermentComponent  {
   
-  constructor(private service:GovListService){}
-  govList:any;
-  ngOnInit():void{
-this.govList=this.service.studGov;
-  }
+  constructor(){}
+//   govList:any;
+//   ngOnInit():void{
+// this.govList=this.service.studGov;
+//   }
 }
   
 // }
